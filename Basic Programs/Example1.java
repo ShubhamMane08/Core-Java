f9c85dd34ef7f929453fe371693920274d4c30bd
ShubhamMane08 <157275319+ShubhamMane08@users.noreply.github.com>
2025-02-06 16:45:36 +0530

    Commit-1
    First Scanner Program

---
+++ Basic Programs/Example1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class Example1
+{
+	public static void main(String [] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a String");
+
+		String str1=sc.nextLine();
+		System.out.println(str1);
+
+	}
+}
