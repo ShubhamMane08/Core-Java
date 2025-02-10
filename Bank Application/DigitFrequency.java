d595293f20fbad4700c7505c0c92f776c4d205e3
ShubhamMane08 <157275319+ShubhamMane08@users.noreply.github.com>
2025-02-10 17:39:24 +0530

    Commit-13
    Digit Frequency
    Six Digits OTP generation

---
+++ Bank Application/DigitFrequency.java
@@ -0,0 +1,26 @@
+class DigitFrequency
+{
+	public static void main(String[] args) 
+	{
+		long num = 1213531343l;
+		System.out.println(num);
+		for(int i =0; i<9;i++)
+		{
+			int cnt=0;
+			for(long j=num;j>0;j/=10)
+			{
+				long rem = j%10;
+				if(i==rem)
+				{
+					cnt++;
+				}
+
+			}
+			if(cnt!=0)
+			{
+				System.out.println(i+" : "+cnt);
+			}
+		}
+		
+	}
+}
