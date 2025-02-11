883a4eb06676de76540155e28e2d2bdef1484e56
ShubhamMane08 <157275319+ShubhamMane08@users.noreply.github.com>
2025-02-11 13:10:07 +0530

    Commit-13
    Max Count

---
+++ Largest&Smallest Digits/CountDgt.java
@@ -0,0 +1,27 @@
+class CountDgt
+{
+	public static void main(String[] args) 
+	{
+		long num = 1214314131657248l;
+		int max = 0,dgt = 0;
+		
+		for(int i=0;i<=9;i++)	
+		{
+			int cnt = 0;
+			for(long j=num;j>0;j/=10)
+			{
+				if(i==(int)(j%10))
+				cnt++;
+			}
+			if(cnt>max)
+			{
+				max=cnt;
+				 dgt =i;
+				
+			}
+		}
+		System.out.println(" max Count is : "+max);
+
+	}
+	
+}
