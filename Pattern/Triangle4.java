29b4c5a86fa5a29ec82e0e34b18553bb97c97bb2
ShubhamMane08 <157275319+ShubhamMane08@users.noreply.github.com>
2025-02-17 22:25:48 +0530

    Commit-30
    All time of Triangles

---
+++ Pattern/Triangle4.java
@@ -0,0 +1,17 @@
+class Triangle4
+{
+	public static void main(String[] args) 
+	{
+		int n=5;
+		for(int i = 1;i<=5;i++)
+		{
+			char ch='a';
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print(ch++ +" ");
+			}
+			System.out.println();
+		}
+		
+	}
+}
