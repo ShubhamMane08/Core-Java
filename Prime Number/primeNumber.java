cfea2083118585e340ab61675e6d19036bb4ceee
ShubhamMane08 <157275319+ShubhamMane08@users.noreply.github.com>
2025-02-23 23:31:19 +0530

    Commit-36
    Prime Number Practice

---
+++ Prime Number/primeNumber.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+class primeNumber
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter a number : ");
+		int num=sc.nextInt();
+			
+		for(int i = 2; i<num;i++)
+		{
+			boolean flag = true;
+
+
+			for(int j=2;j<=(i/2);j++)
+			{
+				if(i%j==0)
+				{
+					flag=false;
+					break;
+				}
+
+			}
+			if(flag)
+			{
+				System.out.print(i+" ");
+			}
+		}
+		
+	}
+	
+}
