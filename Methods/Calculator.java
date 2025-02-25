3dd3b4176051e9b13f527d3068fe866d5c8f74b1
ShubhamMane08 <157275319+ShubhamMane08@users.noreply.github.com>
2025-02-25 12:26:43 +0530

    Commit-40
    Calculator Updated

--- Methods/Calculator.java
+++ Methods/Calculator.java
@@ -1,14 +1,19 @@
 import java.util.Scanner;
 class Calculator
 {
+
 	public static void main(String[] args) 
 	{
 		addition();
 		subtraction();
+		division();
+		multiplication();
+		Mod();
 
 	}
 	public static void addition()
 	{
+		System.out.println("    Addition   ");
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Enter First Number : ");
 		int a=sc.nextInt();
@@ -16,9 +21,11 @@ class Calculator
 		int b=sc.nextInt();
 		int answer=a+b;
 		System.out.println("Addition is "+answer);
+		System.out.println();
 	}
 	public static void subtraction()
 	{
+		System.out.println("   Subtraction   ");
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Enter First Number : ");
 		int a=sc.nextInt();
@@ -26,6 +33,48 @@ class Calculator
 		int b=sc.nextInt();
 		int answer=a-b;
 		System.out.println(a+" - "+b+" = "+answer);
+		System.out.println();
+	}
+	public static void division()
+	{
+		System.out.println("   Division  ");
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter First Number : ");
+		int a=sc.nextInt();
+		System.out.print("Enter Second Number : ");
+		int b=sc.nextInt();
+
+		int answer= a/b;
+		System.out.println(a+" / "+b+" = "+answer);
+		System.out.println();
+		
+	}
+	public static void multiplication()
+	{
+		System.out.println("   Multiplication  ");
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter First Number : ");
+		int a=sc.nextInt();
+		System.out.print("Enter Second Number : ");
+		int b=sc.nextInt();
+
+		int answer= a*b;
+		System.out.println(a+" * "+b+" = "+answer);
+		System.out.println();
+
+	}
+	public static void Mod()
+	{
+		System.out.println("   Modulus   ");
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter First Number : ");
+		int a=sc.nextInt();
+		System.out.print("Enter Second Number : ");
+		int b=sc.nextInt();
+
+		int answer= a%b;
+		System.out.println(a+" % "+b+" = "+answer);
+		System.out.println();
 	}
 
 	
