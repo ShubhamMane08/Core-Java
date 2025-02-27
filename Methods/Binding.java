ef52c192769b9371cce76b91d8262bd6beb325fa
ShubhamMane08 <157275319+ShubhamMane08@users.noreply.github.com>
2025-02-27 11:47:18 +0530

    Commit-43
    String Operations

--- Methods/Binding.java
+++ Methods/Binding.java
@@ -5,6 +5,8 @@ class Binding
 		System.out.println("Main starts");
 		add('a',10);
 		System.out.println("Main Ends");
+		cal(7);
+		string();
 	}
 	public static void add(int a,int b)
 	{
@@ -12,5 +14,18 @@ class Binding
 		int res=a+b;
 		System.out.println(res);
 		System.out.println("Add Ends");
+	}
+	public static void cal(int a)
+	{
+		
+		int x=(a*49/a)+(35/7);
+		System.out.println(x);
+	}
+	public static void string()
+	{
+		String str="shubham";
+		System.out.println(str);
+		System.out.println(str.replace('s','v')+" "+ str.length());
+		System.out.println(str.startsWith("sh"));
 	}
 }
