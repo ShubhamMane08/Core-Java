106b6f732598a6869d6b286f1da30bcacb1233e7
ShubhamMane08 <157275319+ShubhamMane08@users.noreply.github.com>
2025-03-03 12:18:06 +0530

    Commit-47
    Rotating Number

--- Methods/RotatingNumber.java
+++ Methods/RotatingNumber.java
@@ -1,4 +1,35 @@
-RotatingNumber
+class RotatingNumber
 {
+	public static void main(String[] args) 
+	{
+		System.out.println(rotate(1234));
+	}
+	public static int rotate(int num)
+	{
+		int last=num%10;
+		num/=10;
+		int ct = count(num);
+		return last*pow(10,ct)+num;
+		//return (num%10)*power(10,(count(num)-1))+num/10)
+	}
+	public static int count(int num)
+	{
+		int ct=0;
+		while(num!=0)
+		{
+			num/=10;
+			ct++;
+		}
+		return ct;
+	}
+	public static int pow(int digit,int ct)
+	{
+		int pow=1;
+		for(int i=0;i<ct;i++)
+		{
+			pow*=digit;
+		}
+		return pow;
+	}
 	
 }
