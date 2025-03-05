5d843bc152d016ca48f74994d63852b23970f8ce
ShubhamMane08 <157275319+ShubhamMane08@users.noreply.github.com>
2025-03-05 11:35:49 +0530

    Commit-53
    Sum of Digits using another methods using Recursion

---
+++ Recursion/RecSumOfDigit1.java
@@ -0,0 +1,19 @@
+class RecSumOfDigit1
+{
+	public static void main(String[] args) 
+	{
+		System.out.println(sumOfDigit(153,153,0));
+	}
+	public static boolean sumOfDigit(int num,int temp,int sum)
+	{
+		return temp==0?(num==sum):(sumOfDigit(num,temp/10,sum+=(power(temp%10,count(num,0)))));
+	}
+	public static int count(int num, int ct)
+	{
+		return num==0? ct: count(num/10,++ct);
+	}
+	public static int power(int base,int raise)
+	{
+		return raise==0? 1: base*power(base,raise-1);
+	}
+}
