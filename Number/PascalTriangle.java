23c05cfae688ebc07f6de6636b289df0a4e1fb44
ShubhamMane08 <157275319+ShubhamMane08@users.noreply.github.com>
2025-03-06 01:35:26 +0530

    Commit-54
    PascalTriangle

---
+++ Number/PascalTriangle.java
@@ -0,0 +1,32 @@
+//Error
+class PascalTriangle
+{
+	public static void main(String[] args) 
+	{
+		System.out.print(pascalTriangle(8));
+	}
+	public static void pascalTriangle(int n)
+	{
+		for(int i=0;i<n;i++)
+		{
+			for(int space=0; space<n-i-1;space++)
+			{
+				System.out.print(" "+" ");
+			}
+			for(int star=0;star<=i;star++)
+			{
+				System.out.print(fact(i)/fact(star)*fact(i-star)+" ");
+			}
+			System.out.println();
+		}
+	}
+	public static int fact(int num)
+	{
+		int fact=1;
+		for(int i = num; i>0;i--)
+		{
+			fact*=i;
+		}
+		return fact;
+	}
+}
