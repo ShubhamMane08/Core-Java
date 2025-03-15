3db9c13c18748645127285552c8f36b5f1bcd5fe
ShubhamMane08 <157275319+ShubhamMane08@users.noreply.github.com>
2025-03-15 23:33:47 +0530

    Update
    Changes

--- Leap Year/LeapYear.java
+++ Leap Year/LeapYear.java
@@ -7,7 +7,8 @@ class LeapYear
 
 		System.out.print(" Enter the Year : ");
 		int year=sc.nextInt();
-			//2000       //2000
+			//2000       //2000 false
+			//2004                       //true 
 		if((year%4==0 && year%100!=0) || (year%400==0 && year%100!=0))
 		{
 			System.out.println(year+" is a Leap Year");
