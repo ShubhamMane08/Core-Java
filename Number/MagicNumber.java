17300fb26ca3bbe9b7b6c7d8e6f75af8ff39c4bf
ShubhamMane08 <157275319+ShubhamMane08@users.noreply.github.com>
2025-03-15 23:40:14 +0530

    Update
    Tracing

--- Number/MagicNumber.java
+++ Number/MagicNumber.java
@@ -6,13 +6,13 @@ class MagicNumber
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Enter a Number : ");
 		int num = sc.nextInt();
-		//1234
+		//1234 
 		System.out.println(isMagic(num,sumOfDigit(num,0)));
 		
 	}
 	public static boolean isMagic(int num,int sum)
 	{
-		//
+		//false
 
 		if(sum==1)
 		{
@@ -28,9 +28,11 @@ class MagicNumber
 	
 	public static int sumOfDigit(int num,int sum)
 	{
+		//1234 false
 		if(num==0)
 			return sum;
 		else 
 			return sumOfDigit(num/10,sum+=num%10);
+		//123, 1
 	}
 }
