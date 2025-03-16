d85aa2ec740efa619e67d9a344e2e505a4388df3
ShubhamMane08 <157275319+ShubhamMane08@users.noreply.github.com>
2025-03-16 16:15:49 +0530

    Update
    Tracing

--- Methods/RangeOfPrime.java
+++ Methods/RangeOfPrime.java
@@ -5,10 +5,12 @@ class RangeOfPrime
 	{
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Enter the Range : ");
+		//20
 		int num = sc.nextInt();
-
+		//     1    true 
 		for(int start=1;start<=num;start++)
 		{
+			//
 			if(isPrime(start))
 			{
 				System.out.print(start+" ");
@@ -17,11 +19,13 @@ class RangeOfPrime
 
 		
 	}
+								//20
 	public static boolean isPrime(int num)
 	{
-		
+				// 2 true
 		for(int den = 2; den<num;den++)
 		{
+			//20%2  true
 			if(num%den==0)
 			{
 				return false;
