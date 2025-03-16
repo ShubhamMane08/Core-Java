9dfdd585ab528045d1d07edbeef1bd6d748600fe
ShubhamMane08 <157275319+ShubhamMane08@users.noreply.github.com>
2025-03-16 16:09:37 +0530

    Update
    Tracing

--- Looping/ForExample.java
+++ Looping/ForExample.java
@@ -2,12 +2,25 @@ class ForExample
 {
 	public static void main(String[] args)
 	{
-
+			//1  true     1(2)
+			//2  true     2(3)
+		    //3 true      3(4)
+			//4 true      4(5)
+			//5 true      5(6)
+			//6 true 	  6(7)
+			//7 true      7(8)
+			//8 true 	  8(9)
+			//9 true  	  9(10)
+			//10 true 	  10(11)
+			//11 false
 		for(int i=1;i<=10;i++)
 		{
+			//1 2 3 4 5 6 7 8 9 10 
 			System.out.print(i+" ");
 		}
 		
+		//1 2 3 4 5 6 7 8 9 10 
+		//|
 
 		System.out.println();
 
