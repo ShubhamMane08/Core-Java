c0efa055a09250dca2714aae565ea69067f24234
ShubhamMane08 <157275319+ShubhamMane08@users.noreply.github.com>
2025-03-22 11:38:37 +0530

    Commit-63
    Static Examples

---
+++ static/StatVar4.java
@@ -0,0 +1,18 @@
+class StatVar4
+{
+	static String s;
+	public static void main(String[] args)
+	{
+		System.out.println(s);
+		demo();
+		String s="Shubham";
+		System.out.println(s);
+		
+	}
+	public static void demo()
+	{
+		
+		s="Shubham";
+		System.out.println(s);
+	}
+}
