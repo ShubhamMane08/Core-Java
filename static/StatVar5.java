fef82bcd75ab18017156a371246e37dc8046e3a3
ShubhamMane08 <157275319+ShubhamMane08@users.noreply.github.com>
2025-03-22 11:54:45 +0530

    Commit-64
    Static Variables are without compiling cant be used

---
+++ static/StatVar5.java
@@ -0,0 +1,13 @@
+class StatVar5
+{
+	static float f;
+	static char ch;
+	public static void main(String[] args) {
+		System.out.println(f);
+		System.out.println(ch);
+		System.out.println(StatVar4.s);
+		System.out.println(StatVar2.balance);
+
+		
+	}
+}
