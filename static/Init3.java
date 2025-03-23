35880baa1bdd3957f45d3d8c1acf5e623cb82df3
ShubhamMane08 <157275319+ShubhamMane08@users.noreply.github.com>
2025-03-23 12:33:18 +0530

    Commit-66
    Static Method Changes

---
+++ static/Init3.java
@@ -0,0 +1,17 @@
+class Init3
+{
+	static 
+	{
+		test();
+
+	}
+	public static void main(String[] args) 
+	{
+		System.out.println("From Main..!");
+		
+	}
+	public static void test()
+	{
+		System.out.println("I am in Test Method.!!");
+	}
+}
