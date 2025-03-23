77581ad3c36260609f0c45242f17018e69275165
ShubhamMane08 <157275319+ShubhamMane08@users.noreply.github.com>
2025-03-23 12:24:58 +0530

    Commit-64
    Static Block Concept

---
+++ static/Initi1.java
@@ -0,0 +1,18 @@
+class Initi1
+{
+	static
+	{
+		System.out.println("From Static 1");
+
+	}
+	public static void main(String[] args) 
+	{
+		System.out.println("From Main");
+
+	}
+	static
+	{
+		System.out.println("From Static 2");
+
+	}
+}
