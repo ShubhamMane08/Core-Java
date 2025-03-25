0a3d97c9a4a985aeb01be0231b0a316fe5b95cc4
ShubhamMane08 <157275319+ShubhamMane08@users.noreply.github.com>
2025-03-25 11:39:02 +0530

    Commit-69
    Multiple Classes  in a single java file

---
+++ MethodOverloading/Multipleclass.java
@@ -0,0 +1,19 @@
+class Multipleclass
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("From class Multipleclass");
+	}
+}
+class B
+{
+	public static void main(String[] args) {
+		System.out.println("From class B");
+	}
+}
+class C
+{
+	public static void main(String[] args) {
+		System.out.println("From class C");
+	}
+}
