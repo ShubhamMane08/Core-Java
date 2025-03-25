fc0335761fc1cebcf83b2164ba80075f3eaab36e
ShubhamMane08 <157275319+ShubhamMane08@users.noreply.github.com>
2025-03-25 11:44:22 +0530

    Commit-70
    Public Class
    Must give the public class name to the Java File

---
+++ MethodOverloading/B.java
@@ -0,0 +1,25 @@
+class Publicclass
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("From Publicclass ");
+		
+	}
+	
+}
+public class B
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("From Public Class B");
+		
+	}
+}
+class C
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("From Class C");
+		
+	}
+}
