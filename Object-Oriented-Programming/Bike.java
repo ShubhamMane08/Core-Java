0b7095654a0c302fa7222a1639a1bd4753698ea6
ShubhamMane08 <157275319+ShubhamMane08@users.noreply.github.com>
2025-03-28 00:22:45 +0530

    Commit-75
    Bike

---
+++ Object-Oriented-Programming/Bike.java
@@ -0,0 +1,22 @@
+class Bike
+{
+	String brand;
+	String name;
+	double price;
+	int milage;
+	int cc;
+
+
+
+	public void details()
+	{
+		String name="Chapri Bike";
+		System.out.println("Brand : "+brand);
+		System.out.println("Name : "+name);
+		System.out.println("Name : "+this.name);
+		System.out.println("CC : "+cc);
+		System.out.println("Milage : "+milage);
+		System.out.println("Price : "+price);
+
+	}
+}
---
+++ Object-Oriented-Programming/BikeDriver.java
@@ -0,0 +1,16 @@
+class BikeDriver
+{
+	public static void main(String[] args) 
+	{
+		Bike b1=new Bike();
+
+		b1.brand="KTM";
+		b1.name="Duke 360";
+		b1.cc=360;
+		b1.milage=20;
+		b1.price=450000.0;
+		b1.details();
+
+		
+	}
+}
