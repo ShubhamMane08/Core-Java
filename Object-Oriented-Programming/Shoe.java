0f824fe3fe0300ad6cc6611165d09f4ad2f7897f
ShubhamMane08 <157275319+ShubhamMane08@users.noreply.github.com>
2025-03-29 23:18:11 +0530

    Commit-77
    Non-static Initializers

---
+++ Object-Oriented-Programming/Shoe.java
@@ -0,0 +1,17 @@
+class Shoe
+{
+	String brand="Nike";
+	String type;
+	int size;
+	double price;
+
+
+	public void display()
+	{
+		System.out.println("Brand : "+brand);
+		System.out.println("Type : "+type);
+		System.out.println("Size : "+size);
+		System.out.println("Price : "+price);
+	}
+
+}
---
+++ Object-Oriented-Programming/ShoeDriver.java
@@ -0,0 +1,12 @@
+class ShoeDriver
+{
+	public static void main(String[] args) 
+	{
+		Shoe sh=new Shoe();
+		sh.type="Sports";
+		sh.size=8;
+		sh.price=2000.0;
+		sh.display();
+		
+	}
+}
