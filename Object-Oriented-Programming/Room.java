aaeff9c9c783537721baa61fec711e4aab8e39b2
ShubhamMane08 <157275319+ShubhamMane08@users.noreply.github.com>
2025-03-29 23:51:56 +0530

    Commit-78
    Room Rent

---
+++ Object-Oriented-Programming/Room.java
@@ -0,0 +1,20 @@
+class Room
+{
+	String apartment="Ghulmohor";
+	int floor;
+	String type;
+	String owner;
+	double rent;
+	String parking;
+
+
+	public void details()
+	{
+		System.out.println("Apartment : "+apartment);
+		System.out.println("Floor : "+floor);
+		System.out.println("Room Type : "+type);
+		System.out.println("Owner Name : "+owner);
+		System.out.println("Rent : "+rent);
+		System.out.println("Parking : "+parking);
+	}
+}
---
+++ Object-Oriented-Programming/RoomDriver.java
@@ -0,0 +1,15 @@
+class RoomDriver
+{
+	public static void main(String[] args) 
+	{
+		Room rm = new Room();
+		rm.type="3BHK";
+		rm.floor=11;
+		rm.owner="Shubham";
+		rm.parking="Availabe";
+		rm.rent=7000.0;
+		rm.details();
+
+		
+	}
+}
