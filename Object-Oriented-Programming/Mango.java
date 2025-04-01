d26f64044d0306b887bb85bb0f7c17505e4fcdfc
ShubhamMane08 <157275319+ShubhamMane08@users.noreply.github.com>
2025-04-01 23:42:12 +0530

    Commit-82
    Example of Chaining of Methods

---
+++ Object-Oriented-Programming/Mango.java
@@ -0,0 +1,23 @@
+class Mango
+{
+	String type;
+	String origin;
+	double price;
+
+
+	public Mango printPrice()
+	{
+		System.out.println(price);
+		return this;
+	}
+	public Mango printOrigin()
+	{
+		System.out.println(origin);
+		return this;
+	}
+	public Mango printType()
+	{
+		System.out.println(type);
+		return this;
+	}
+}
