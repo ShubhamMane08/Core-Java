939750311c222248ca209f582d7520b0c5c09fa1
ShubhamMane08 <157275319+ShubhamMane08@users.noreply.github.com>
2025-04-05 23:57:19 +0530

    changes

--- Object-Oriented-Programming/src/Test.java
+++ Object-Oriented-Programming/src/Test.java
@@ -1,12 +1,18 @@
 class Test
 {
 	int a=100;
-	char = 'a';
+	char ch = 'a';
 
 	static 
 	{
 		System.out.println("From static block 1");
-		
+		System.out.println(a);
+
+	}
+	static 
+	{
+		System.out.println("From static block 2");
+		System.out.println(ch);
 	}
 
 }
