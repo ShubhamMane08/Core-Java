66c3c044f921e34bd07ba9f363ca7030c20820bb
ShubhamMane08 <157275319+ShubhamMane08@users.noreply.github.com>
2025-04-08 01:19:04 +0530

    Commit-91

--- Object-Oriented-Programming/src/Car.java
+++ Object-Oriented-Programming/src/Car.java
@@ -10,14 +10,13 @@ class Car
 
 	Car()
 	{
-		
+
 	}
 
 	public void details()
 	{
 		String model=" (Kidnaper Car)";
 		System.out.println("Brand : "+brand);
-		System.out.println("Model : "+this.model+model);
 		System.out.println("Milage : "+milage);
 		System.out.println("Mode of Fuel : "+modeOfFuel);
 		System.out.println("Topspeed : "+topSpeed);
