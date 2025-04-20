9801758817777e91ab10db14eadb9be3874f3e0c
Shubham Mane <shubhammanecr7@gmail.com>
2025-04-20 12:00:49 +0530

    changes

--- E8/src/PrinciplesofObjectoriented/Cabinet.java
+++ E8/src/PrinciplesofObjectoriented/Cabinet.java
@@ -7,7 +7,7 @@ public class Cabinet {
 	Motherboard mb;
 	HardDisk hdd;
 	Monitor mm;
-//	Keyboard kb;
+	Keyboard kb;
 	
 	
 	Cabinet()
@@ -16,11 +16,12 @@ public class Cabinet {
 	}
 
 
-	public Cabinet(String startbutton, int ports,String brand, int register, String coolingfan, String processor, int diode, double price, String capacity, int speed  ,String brand1,double resolution,int size,double price1) {
+	public Cabinet(String startbutton, int ports,String brand, int register, String coolingfan, String processor, int diode, double price, String capacity, int speed  ,String brand1,double resolution,int size,
+			double price1,String brand2,String type,double price3) {
 		mb= new Motherboard(register, coolingfan, processor, diode, price);
 		hdd=new HardDisk(brand, capacity, speed);
 		mm=new Monitor(brand1,resolution,size,price1);
-//		kb=new Keyboard()
+		kb=new Keyboard(brand2,type,price3);
 		this.startbutton = startbutton;
 		this.ports = ports;
 		
--- E8/src/PrinciplesofObjectoriented/ComputerDriver.java
+++ E8/src/PrinciplesofObjectoriented/ComputerDriver.java
@@ -3,7 +3,7 @@ package PrinciplesofObjectoriented;
 public class ComputerDriver {
 	public static void main(String[] args) {
 		
-		Cabinet cb = new Cabinet("ON", 7, "Dell", 72, "ROG", "intel i7 11th gen", 85, 20000.0, "1000", 200,"Logitech",1600.0,32,5000.0);
+		Cabinet cb = new Cabinet("ON", 7, "Dell", 72, "ROG", "intel i7 11th gen", 85, 20000.0, "1000", 200,"Logitech",1600.0,32,5000.0,"Logitech,","Wired",1000.0);
 		cb.DisplayCPU();
 	}
 }
