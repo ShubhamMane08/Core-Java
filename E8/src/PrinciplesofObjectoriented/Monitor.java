f6a4a1ee138530e6085bc33e9e238424bf35a61d
Shubham Mane <shubhammanecr7@gmail.com>
2025-04-20 11:19:06 +0530

    Creating Monitor Class

---
+++ E8/src/PrinciplesofObjectoriented/Monitor.java
@@ -0,0 +1,23 @@
+package PrinciplesofObjectoriented;
+
+public class Monitor {
+	String brand;
+	double resolution;
+	int size;
+	double price;
+	
+	
+	
+	Monitor()
+	{
+		
+	}
+	public Monitor(String brand, double resolution, int size,double price)
+	{
+		this.brand=brand;
+		this.resolution=resolution;
+		this.size=size;
+		this.price=price;
+	}
+
+}
