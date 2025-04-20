26bb6a74493efd816fb62ba8cc9a3fb7b987bcf9
Shubham Mane <shubhammanecr7@gmail.com>
2025-04-20 11:37:24 +0530

    changes

--- E8/src/PrinciplesofObjectoriented/Cabinet.java
+++ E8/src/PrinciplesofObjectoriented/Cabinet.java
@@ -7,6 +7,7 @@ public class Cabinet {
 	Motherboard mb;
 	HardDisk hdd;
 	Monitor mm;
+//	Keyboard kb;
 	
 	
 	Cabinet()
@@ -15,9 +16,11 @@ public class Cabinet {
 	}
 
 
-	public Cabinet(String startbutton, int ports,String brand, int register, String coolingfan, String processor, int diode, double price, String capacity, int speed  ) {
+	public Cabinet(String startbutton, int ports,String brand, int register, String coolingfan, String processor, int diode, double price, String capacity, int speed  ,String brand1,double resolution,int size,double price1) {
 		mb= new Motherboard(register, coolingfan, processor, diode, price);
 		hdd=new HardDisk(brand, capacity, speed);
+		mm=new Monitor(brand1,resolution,size,price1);
+//		kb=new Keyboard()
 		this.startbutton = startbutton;
 		this.ports = ports;
 		
