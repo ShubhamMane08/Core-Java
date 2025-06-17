88c2880a54f6e02763424595686961b7856baff5
Shubham Mane <shubhammanecr7@gmail.com>
2025-06-18 00:22:31 +0530

    changes

---
+++ E8/src/ObjectClass/StudentDriver.java
@@ -0,0 +1,11 @@
+package ObjectClass;
+
+public class StudentDriver {
+	public static void main(String[] args) {
+		
+		Student s1 = new Student("Raju","FC",123,"BE","CS");
+		System.out.println(s1.toString());
+		System.out.println(s1);
+	}
+
+}
