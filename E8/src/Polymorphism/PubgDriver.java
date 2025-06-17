c057d7c1635a946b96f8c662c88b40988ce73800
Shubham Mane <shubhammanecr7@gmail.com>
2025-06-18 00:11:44 +0530

    Changes in the Polymorphism

---
+++ E8/src/Polymorphism/PubgDriver.java
@@ -0,0 +1,15 @@
+package Polymorphism;
+
+public class PubgDriver {
+	
+	public static void main(String[] args) {
+		
+		Pubg p1 = new Pubg1();
+		p1.fire();
+		p1.turnLeft();
+		p1.turnRight();
+		
+	
+	}
+
+}
