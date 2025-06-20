f5bf18b28e0360cb7b8f2ad5d5d9ab72e70d7bae
Shubham Mane <shubhammanecr7@gmail.com>
2025-06-20 16:36:43 +0530

    Practicing Patterns

---
+++ E8/src/Pattern/Pattern1.java
@@ -0,0 +1,18 @@
+package Pattern;
+
+public class Pattern1 {
+	public static void main(String[] args) {
+		
+		int num=4;
+		
+		for(int i=0;i<=num;i++)
+		{
+			for(int j=0;j<num;j++)
+			{
+				System.out.print("*"+" ");
+			}
+			System.out.println();
+		}
+	}
+
+}
