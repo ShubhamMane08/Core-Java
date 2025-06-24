f1796fa0b2611d07342bd5e122fb3ce9f26a3092
Shubham Mane <shubhammanecr7@gmail.com>
2025-06-25 01:19:31 +0530

    practice

---
+++ E8/src/Arrays/Practice.java
@@ -0,0 +1,21 @@
+package Arrays;
+
+public class Practice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int [] marks = new int [5];
+		System.out.println(marks);
+		
+		int [] mark = {100,200};
+		System.out.println(mark.length);
+		
+		for (int i = 0; i<mark.length; i++)
+		{
+			System.out.println(mark[i]);
+		}
+	}
+	
+
+}
