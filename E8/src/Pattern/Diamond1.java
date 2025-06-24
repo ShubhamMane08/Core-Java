d53767b7def8297f34c2a6fc02e7a2e9aa3dd028
Shubham Mane <shubhammanecr7@gmail.com>
2025-06-24 13:47:41 +0530

    changes

---
+++ E8/src/Pattern/Diamond1.java
@@ -0,0 +1,34 @@
+package Pattern;
+
+public class Diamond1 {
+
+	public static void main(String[] args) {
+		
+		int n = 7 , space=n/2, star=1;
+		
+		for(int i =0; i<n;i++)
+		{
+			for(int j=0; j<space;j++)
+			{
+				System.out.print(" "+" ");
+			}
+			for(int j=0;j<star;j++)
+			{
+				System.out.print("*"+" ");
+			}
+			System.out.println();
+			
+			if(i<n/2)
+			{
+				space--;
+				star+=2;
+			}
+			else
+			{
+				space++;
+				star-=2;
+			};
+			
+		}
+	}
+}
