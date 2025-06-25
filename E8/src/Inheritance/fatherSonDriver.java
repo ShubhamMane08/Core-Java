574a2731500f35f5c5271117b5c819c30834ac76
Shubham Mane <shubhammanecr7@gmail.com>
2025-06-26 00:15:15 +0530

    Inheritance

---
+++ E8/src/Inheritance/fatherSonDriver.java
@@ -0,0 +1,12 @@
+package Inheritance;
+
+public class fatherSonDriver {
+
+	public static void main(String[] args) {
+		
+		
+		Son s = new Son("Santro","1BHK","39","Ramesh","1st");
+		s.displaySon();
+//		s.displayFather();
+	}
+}
