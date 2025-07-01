fa74aab2f6543bd50cf68eb1e66df224629d5b99
Shubham Mane <shubhammanecr7@gmail.com>
2025-07-01 22:31:44 +0530

    Changes

---
+++ E8/src/Exception/CheckedExceptions.java
@@ -0,0 +1,18 @@
+package Exception;
+
+public class CheckedExceptions {
+
+	public static void main(String[] args)  throws InterruptedException{
+		// TODO Auto-generated method stub
+
+	
+		for(int i =1; i<=5;i++)
+		{
+			System.out.println("I love Bharat");
+			Thread.sleep(2000);
+		}
+		
+	}
+	
+
+}
