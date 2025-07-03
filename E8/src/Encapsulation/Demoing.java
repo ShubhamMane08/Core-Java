10cf8c313360a803f778acb0efacaa3256dce999
Shubham Mane <shubhammanecr7@gmail.com>
2025-07-04 01:56:34 +0530

    changes

---
+++ E8/src/Encapsulation/Demoing.java
@@ -0,0 +1,20 @@
+package Encapsulation;
+
+public class Demoing {
+	
+	Demoing()
+	{
+		
+	}
+	private void confidential()
+	{
+		System.out.println("..... .... .... ... .. .......");
+	}
+	
+	
+	public void getConfi()
+	{
+		confidential();
+	}	
+}
+
