48abc23f7d54f7f8e6b892f34c8e33f2fbea083d
Shubham Mane <shubhammanecr7@gmail.com>
2025-07-07 22:18:15 +0530

    changes

---
+++ E8/src/Encapsulation/DinguLoveLifeDriver.java
@@ -0,0 +1,14 @@
+package Encapsulation;
+
+public class DinguLoveLifeDriver {
+	public static void main(String[] args) {
+		
+		
+		DinguLoveLife love =  DinguLoveLife.createObject();
+		System.out.println(love.gf1);
+		System.out.println(love.gf2);
+		System.out.println(love.gf3);
+		;
+	}
+
+}
