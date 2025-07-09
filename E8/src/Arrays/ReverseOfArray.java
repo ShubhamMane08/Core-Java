98ce7b37800c0336abdf3a562d7002509e53666f
Shubham Mane <shubhammanecr7@gmail.com>
2025-07-09 14:42:48 +0530

    Practice

---
+++ E8/src/Arrays/ReverseOfArray.java
@@ -0,0 +1,19 @@
+package Arrays;
+
+import java.util.Arrays;
+
+public class ReverseOfArray {
+
+		
+	public static void main(String[] args) {
+		
+		int []arr= {1,2,3,4,5};
+		int []rev = new int [arr.length];
+		
+		for(int i=0;i<arr.length;i++)
+		{
+			rev[arr.length-1-i]=arr[i];
+		}
+		System.out.println(Arrays.toString(rev));
+	}
+}
