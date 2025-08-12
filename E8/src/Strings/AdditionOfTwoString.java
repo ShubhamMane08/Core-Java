96d1bf1eb729ae59cc1279334e29eabfe2ef969b
Shubham Mane <shubhammanecr7@gmail.com>
2025-08-13 00:14:25 +0530

    Changes

---
+++ E8/src/Strings/AdditionOfTwoString.java
@@ -0,0 +1,21 @@
+package Strings;
+
+import java.util.Scanner;
+
+public class AdditionOfTwoString {
+	
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		String a=sc.next();
+		String b = sc.next();
+		
+		int sum =a.length()+b.length();
+		System.out.println(sum);
+		
+	}
+
+
+
+}
